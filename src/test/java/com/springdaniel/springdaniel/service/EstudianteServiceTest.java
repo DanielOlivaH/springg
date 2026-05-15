0e9c32e41f8cb232eccc3a87b177086af3b1d05e
DanielOlivaH <damsjo10122004@gmail.com>
2026-05-15 15:48:18 +0100

    prueba exito

--- src/test/java/com/springdaniel/springdaniel/service/EstudianteServiceTest.java
+++ src/test/java/com/springdaniel/springdaniel/service/EstudianteServiceTest.java
@@ -39,7 +39,7 @@ public class EstudianteServiceTest {
         when(repository.save(any(Estudiante.class))).thenReturn(estudiante);
         Estudiante guardado = service.guardarEstudiante(new Estudiante());
         assertNotNull(guardado);
-        assertEquals("Pepe Perez", guardado.getNombre());
+        assertEquals("Juan Perez", guardado.getNombre());
         verify(repository, times(1)).save(any(Estudiante.class));
     }
 

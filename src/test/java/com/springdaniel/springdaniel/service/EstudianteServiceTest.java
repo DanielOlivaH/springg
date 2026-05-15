96fd67c27d95b8b41776b62b6d2217d89225ad6d
DanielOlivaH <damsjo10122004@gmail.com>
2026-05-15 15:45:24 +0100

    prueba error

--- src/test/java/com/springdaniel/springdaniel/service/EstudianteServiceTest.java
+++ src/test/java/com/springdaniel/springdaniel/service/EstudianteServiceTest.java
@@ -39,7 +39,7 @@ public class EstudianteServiceTest {
         when(repository.save(any(Estudiante.class))).thenReturn(estudiante);
         Estudiante guardado = service.guardarEstudiante(new Estudiante());
         assertNotNull(guardado);
-        assertEquals("Juan Perez", guardado.getNombre());
+        assertEquals("Pepe Perez", guardado.getNombre());
         verify(repository, times(1)).save(any(Estudiante.class));
     }
 

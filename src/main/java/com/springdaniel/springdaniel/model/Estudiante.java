30bea55a609efe226d94f62a6687f589eb8ef081
DanielOlivaH <damsjo10122004@gmail.com>
2026-05-15 14:46:14 +0100

    añadido tests unitarios para servicio y controlador

--- src/main/java/com/springdaniel/springdaniel/model/Estudiante.java
+++ src/main/java/com/springdaniel/springdaniel/model/Estudiante.java
@@ -1,16 +1,24 @@
 package com.springdaniel.springdaniel.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
 @Table(name = "estudiantes")
-@Data
 public class Estudiante {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String email;
     private int edad;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
 }
